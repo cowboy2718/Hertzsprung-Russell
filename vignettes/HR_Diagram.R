c1f0934de08aea443b6482f9bf1673a564e852ab
Tony Gojanovic <tonygojanovic@Tonys-MacBook-Pro.local>
2018-11-24 20:23:43 -0700

    modified documents

---
+++ vignettes/HR_Diagram.R
@@ -0,0 +1,34 @@
+## ----warning=FALSE, message=FALSE----------------------------------------
+library(HRDiagram)
+library(ggplot2)
+library(dplyr)
+library(readxl)
+library(GDAdata)
+
+
+## ----message=FALSE, eval=FALSE-------------------------------------------
+#  library(readxl)
+#  df<-import_mcdonald_data("mcdonaldstars.xlsx")
+
+## ----message=FALSE, eval=FALSE-------------------------------------------
+#  install.packages("GDAdata")
+#  data(HRstars, package="GDAdata")
+
+## ----message=FALSE, eval=FALSE-------------------------------------------
+#  library(dplyr)
+#  spectral(filename)
+
+## ----message=FALSE, eval=FALSE-------------------------------------------
+#  library(dplyr)
+#  library(ggplot2)
+#  library(plotly)
+#  interactiveHR(filename) #Filename will be the imported McDonald Observatory dataset
+
+## ----message=FALSE, eval=FALSE-------------------------------------------
+#  library(dplyr)
+#  library(ggplot2)
+#  library(plotly)
+#  install.packages("GDAdata")
+#  data(HRstars, package="GDAdata")
+#  yale_hr_diagram
+
